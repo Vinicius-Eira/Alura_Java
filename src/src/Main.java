d16835175afce61df7e03e9e926c7521621eb4b8
Vinícius Ribeiro Dos Santos Eira <161368325+Vinicius-Eira@users.noreply.github.com>
2024-09-13 16:59:02 -0300

    Add files via upload

---
+++ src/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8) / 3;
+
+    }
+}
